13e2a6974bdcc184c562b6acebaa3f7754ab0fc8
Omkar Shirse <112716128+OmShirse@users.noreply.github.com>
2026-05-05 20:44:36 +0530

    Refactor chessboard drawing logic for clarity

--- chess.py
+++ chess.py
@@ -6,18 +6,20 @@ screen = pygame.display.set_mode((1080, 1080))
 running = True
 while running:
     screen.fill((255, 255, 255))
-    for j in range(50,750,200):
-        for i in range(100,900,200):
-            pygame.draw.rect(screen, (0, 0, 0), (i, j, 100, 100))
-        for i in range(200,900,200):
-            pygame.draw.rect(screen, (180, 180, 180), (i, j, 100, 100))
+    
+    for column in range(50,750,200):
+        for row in range(100,900,200):
+            pygame.draw.rect(screen, (0, 0, 0), (row, column, 100, 100))
+        for row in range(200,900,200):
+            pygame.draw.rect(screen, (180, 180, 180), (row, column, 100, 100))
 
-    for j in range(150,750,200):
-        for i in range(200,900,200):
-            pygame.draw.rect(screen, (0, 0, 0), (i, j, 100, 100))
-        for i in range(100,900,200):
-            pygame.draw.rect(screen, (180, 180, 180), (i, j, 100, 100))
-    for event in pygame.event.get():
+    for column in range(150,750,200):
+        for row in range(200,900,200):
+            pygame.draw.rect(screen, (0, 0, 0), (row, column, 100, 100))
+        for row in range(100,900,200):
+            pygame.draw.rect(screen, (180, 180, 180), (row, column, 100, 100))
+
+for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
