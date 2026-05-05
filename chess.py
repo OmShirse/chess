80afc7ac8be58a5672ec70386fe3f85b6f375de1
Omkar Shirse <112716128+OmShirse@users.noreply.github.com>
2026-05-05 20:36:34 +0530

    Refactor rectangle drawing using loops

--- chess.py
+++ chess.py
@@ -6,93 +6,17 @@ screen = pygame.display.set_mode((1080, 1080))
 running = True
 while running:
     screen.fill((255, 255, 255))
-#1
-    pygame.draw.rect(screen, (0, 0, 0), (100, 50, 100, 100))
-    pygame.draw.rect(screen, (180, 180, 180), (200, 50, 100, 100))
-    pygame.draw.rect(screen, (0, 0, 0), (300, 50, 100, 100))
-    pygame.draw.rect(screen, (180, 180, 180), (400, 50, 100, 100))
-    pygame.draw.rect(screen, (0, 0, 0), (500, 50, 100, 100))
-    pygame.draw.rect(screen, (180, 180, 180), (600, 50, 100, 100))
-    pygame.draw.rect(screen, (0, 0, 0), (700, 50, 100, 100))
-    pygame.draw.rect(screen, (180, 180, 180), (800, 50, 100, 100))
-    pygame.draw.rect(screen, (0, 0, 0), (900, 50, 100, 100))
+    for j in range(50,750,200):
+        for i in range(100,900,200):
+            pygame.draw.rect(screen, (0, 0, 0), (i, j, 100, 100))
+        for i in range(200,900,200):
+            pygame.draw.rect(screen, (180, 180, 180), (i, j, 100, 100))
 
-#2
-    pygame.draw.rect(screen, (180, 180, 180), (100, 150, 100, 100))
-    pygame.draw.rect(screen, (0, 0, 0), (200, 150, 100, 100))
-    pygame.draw.rect(screen, (180, 180, 180), (300, 150, 100, 100))
-    pygame.draw.rect(screen, (0, 0, 0), (400, 150, 100, 100))
-    pygame.draw.rect(screen, (180, 180, 180), (500, 150, 100, 100))
-    pygame.draw.rect(screen, (0, 0, 0), (600, 150, 100, 100))
-    pygame.draw.rect(screen, (180, 180, 180), (700, 150, 100, 100))
-    pygame.draw.rect(screen, (0, 0, 0), (800, 150, 100, 100))
-    pygame.draw.rect(screen, (180, 180, 180), (900, 150, 100, 100))    
-#3
-    pygame.draw.rect(screen, (0, 0, 0), (100, 250, 100, 100))
-    pygame.draw.rect(screen, (180, 180, 180), (200, 250, 100, 100))
-    pygame.draw.rect(screen, (0, 0, 0), (300, 250, 100, 100))
-    pygame.draw.rect(screen, (180, 180, 180), (400, 250, 100, 100))
-    pygame.draw.rect(screen, (0, 0, 0), (500, 250, 100, 100))
-    pygame.draw.rect(screen, (180, 180, 180), (600, 250, 100, 100))
-    pygame.draw.rect(screen, (0, 0, 0), (700, 250, 100, 100))
-    pygame.draw.rect(screen, (180, 180, 180), (800, 250, 100, 100))
-    pygame.draw.rect(screen, (0, 0, 0), (900, 250, 100, 100))
-
-#4
-    pygame.draw.rect(screen, (180, 180, 180), (100, 350, 100, 100))
-    pygame.draw.rect(screen, (0, 0, 0), (200, 350, 100, 100))
-    pygame.draw.rect(screen, (180, 180, 180), (300, 350, 100, 100))
-    pygame.draw.rect(screen, (0, 0, 0), (400, 350, 100, 100))
-    pygame.draw.rect(screen, (180, 180, 180), (500, 350, 100, 100))
-    pygame.draw.rect(screen, (0, 0, 0), (600, 350, 100, 100))
-    pygame.draw.rect(screen, (180, 180, 180), (700, 350, 100, 100))
-    pygame.draw.rect(screen, (0, 0, 0), (800, 350, 100, 100))
-    pygame.draw.rect(screen, (180, 180, 180), (900, 350, 100, 100)) 
-
-#5
-    pygame.draw.rect(screen, (0, 0, 0), (100, 450, 100, 100))
-    pygame.draw.rect(screen, (180, 180, 180), (200, 450, 100, 100))
-    pygame.draw.rect(screen, (0, 0, 0), (300, 450, 100, 100))
-    pygame.draw.rect(screen, (180, 180, 180), (400, 450, 100, 100))
-    pygame.draw.rect(screen, (0, 0, 0), (500, 450, 100, 100))
-    pygame.draw.rect(screen, (180, 180, 180), (600, 450, 100, 100))
-    pygame.draw.rect(screen, (0, 0, 0), (700, 450, 100, 100))
-    pygame.draw.rect(screen, (180, 180, 180), (800, 450, 100, 100))
-    pygame.draw.rect(screen, (0, 0, 0), (900, 450, 100, 100))
-
-#6
-    pygame.draw.rect(screen, (180, 180, 180), (100, 550, 100, 100))
-    pygame.draw.rect(screen, (0, 0, 0), (200, 550, 100, 100))
-    pygame.draw.rect(screen, (180, 180, 180), (300, 550, 100, 100))
-    pygame.draw.rect(screen, (0, 0, 0), (400, 550, 100, 100))
-    pygame.draw.rect(screen, (180, 180, 180), (500, 550, 100, 100))
-    pygame.draw.rect(screen, (0, 0, 0), (600, 550, 100, 100))
-    pygame.draw.rect(screen, (180, 180, 180), (700, 550, 100, 100))
-    pygame.draw.rect(screen, (0, 0, 0), (800, 550, 100, 100))
-    pygame.draw.rect(screen, (180, 180, 180), (900, 550, 100, 100)) 
-
-#7
-    pygame.draw.rect(screen, (0, 0, 0), (100, 650, 100, 100))
-    pygame.draw.rect(screen, (180, 180, 180), (200, 650, 100, 100))
-    pygame.draw.rect(screen, (0, 0, 0), (300, 650, 100, 100))
-    pygame.draw.rect(screen, (180, 180, 180), (400, 650, 100, 100))
-    pygame.draw.rect(screen, (0, 0, 0), (500, 650, 100, 100))
-    pygame.draw.rect(screen, (180, 180, 180), (600, 650, 100, 100))
-    pygame.draw.rect(screen, (0, 0, 0), (700, 650, 100, 100))
-    pygame.draw.rect(screen, (180, 180, 180), (800, 650, 100, 100))
-    pygame.draw.rect(screen, (0, 0, 0), (900, 650, 100, 100))
-
-#8
-    pygame.draw.rect(screen, (180, 180, 180), (100, 750, 100, 100))
-    pygame.draw.rect(screen, (0, 0, 0), (200, 750, 100, 100))
-    pygame.draw.rect(screen, (180, 180, 180), (300, 750, 100, 100))
-    pygame.draw.rect(screen, (0, 0, 0), (400, 750, 100, 100))
-    pygame.draw.rect(screen, (180, 180, 180), (500, 750, 100, 100))
-    pygame.draw.rect(screen, (0, 0, 0), (600, 750, 100, 100))
-    pygame.draw.rect(screen, (180, 180, 180), (700, 750, 100, 100))
-    pygame.draw.rect(screen, (0, 0, 0), (800, 750, 100, 100))
-    pygame.draw.rect(screen, (180, 180, 180), (900, 750, 100, 100)) 
-
+    for j in range(150,750,200):
+        for i in range(200,900,200):
+            pygame.draw.rect(screen, (0, 0, 0), (i, j, 100, 100))
+        for i in range(100,900,200):
+            pygame.draw.rect(screen, (180, 180, 180), (i, j, 100, 100))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
